2fd54a103f6bfc9b1b0e220f6956fba36d90a731
Mustafa Aydin <97643186+Mystical06@users.noreply.github.com>
2023-03-13 14:28:18 +0000

    Testing

--- src/test/java/cbt/Driver.java
+++ src/test/java/cbt/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "52.201.221.215";
+                        String gridAddress = "3.85.16.247";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
